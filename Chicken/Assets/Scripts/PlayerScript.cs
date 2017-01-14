77558e36d1aa8f599b347ad1592a75e9218955a3
Rob <robswor@umich.edu>
2017-01-14 12:32:21 -0500

    prep merge

--- Chicken/Assets/Scripts/PlayerScript.cs
+++ Chicken/Assets/Scripts/PlayerScript.cs
@@ -7,28 +7,37 @@ public class PlayerScript : MonoBehaviour {
 	public bool can_jump;
 	public bool a_disabled;
 	float a_time;
-	public static a_charge;
-	public static a_cooldown;	
+//	public static a_charge;
+//	public static a_cooldown;	
 	public bool b_disabled;
 	float b_time;
-	public static b_charge;
-	public static b_cooldown;
+//	public static b_charge;
+//	public static b_cooldown;
 	public bool x_disabled;
 	float x_time;
-	public static x_charge;
-	public static x_cooldown;
+//	public static x_charge;
+//	public static x_cooldown;
 	public bool y_disabled;
 	float y_time;
-	public static y_charge;
-	public static y_cooldown;
+//	public static y_charge;
+//	public static y_cooldown;
 	Rigidbody rb;
 	public Lazor projectile;
+
+    bool falling = false;
+    bool grounded = false;
+    bool noJumping = false;
+    BoxCollider feet;
+    int jumps = 2;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		x_disabled = true;
 		a_disabled = true;
 		can_attack = true;
+        feet = gameObject.GetComponentInChildren(typeof(BoxCollider)) as BoxCollider;
+        Physics.IgnoreLayerCollision(0, 8, true);
 	}
 	
 	// Update is called once per frame
@@ -54,11 +63,23 @@ public class PlayerScript : MonoBehaviour {
 		var y = -Input.GetAxis("LeftY");
 		transform.Translate(x, 0, 0);
 
-		if(y > 0 && can_jump){
-			rb.AddForce(0, 450f, 0);
-			can_jump = false;
+		if(y > 0 && jumps > 0 && !noJumping)
+        {
+            StartCoroutine("Jump");
+            noJumping = true;
 		}
-
+        if (y < 0)
+        {
+            StartCoroutine("Fall");
+        }
+        if (noJumping && y <= 0)
+            noJumping = false;
+        if (falling && rb.velocity.y == 0)
+        {
+            grounded = true;
+            falling = false;
+        }
+        
 		//Attacks
 		if(can_attack){
 			if(Input.GetButtonDown("A") && !a_disabled){
@@ -86,7 +107,35 @@ public class PlayerScript : MonoBehaviour {
 		}
 	}
 
-	void OnCollisionEnter() {
-		can_jump = true;
+	void OnCollisionEnter(Collider other) {
+     /*  if (rb.velocity.y <= 0 && (other.name.Contains("Plat") || other.name.Contains("Floor")))
+        {
+            jumps = 2;
+        }
+        else if (other.name.Contains("Wall") || other.name.Contains("Ceil"))
+            jumps += 1;*/
 	}
+
+    private IEnumerator Fall()
+    {
+        rb.AddForce(0, -10f, 0);
+        feet.isTrigger = true;
+        Debug.Log("Falling through platform");
+        falling = true;
+        yield return new WaitForSeconds(0.5f);
+        feet.isTrigger = false;
+        Debug.Log("Fall complete");
+    }
+
+    private IEnumerator Jump()
+    {
+        jumps -= 1;
+        rb.velocity = new Vector3(rb.velocity.x, 8f);
+        Debug.Log("Jumping");
+        falling = false;
+        while (!grounded)
+            yield return null;
+        jumps = Mathf.Max(2, jumps + 1);
+        Debug.Log("Jump Complete");
+    }
 }
---
+++ Chicken/Assets/StageScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageScript : MonoBehaviour {
+    public bool isWall;
+    public bool isGrd;
+    public bool isPlat;
+    public bool isCeil;
+}
