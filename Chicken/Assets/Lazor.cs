70e69d2a1ae80339a021d97faf4c48355479e35d
Jacob <marksja@umich.edu>
2017-01-13 22:53:43 -0500

    player script

---
+++ Chicken/Assets/Lazor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazor : MonoBehaviour {
+
+	bool hit;
+	// Use this for initialization
+	void Start () {
+		hit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollsionEnter(){
+		Destroy(this);
+	}
+}
