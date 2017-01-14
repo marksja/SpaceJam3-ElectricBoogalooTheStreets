ff083d3c87a58f272d343665764ded0b42f4c920
Jacob <marksja@umich.edu>
2017-01-13 23:07:33 -0500

    Added cooldowns and charging

--- Chicken/Assets/Scripts/PlayerScript.cs
+++ Chicken/Assets/Scripts/PlayerScript.cs
@@ -3,24 +3,21 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	int last_used;
+	float cool_time;
 	public bool can_attack;
 	public bool can_jump;
 	public bool a_disabled;
-	float a_time;
-	public static a_charge;
-	public static a_cooldown;	
+	public float a_charge;
+	public float a_cooldown;	
 	public bool b_disabled;
-	float b_time;
-	public static b_charge;
-	public static b_cooldown;
+	public float b_charge;
+	public float b_cooldown;
 	public bool x_disabled;
-	float x_time;
-	public static x_charge;
-	public static x_cooldown;
+	public float x_charge;
+	public float x_cooldown;
 	public bool y_disabled;
-	float y_time;
-	public static y_charge;
-	public static y_cooldown;
+	public float y_charge;
+	public float y_cooldown;
 	Rigidbody rb;
 	public Lazor projectile;
 	// Use this for initialization
@@ -34,21 +31,22 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Update can_attack
-		/*switch (last_used){
+		cool_time += Time.deltaTime;
+		switch (last_used){
 			case 0:
 				if(cool_time > a_cooldown) can_attack = true;
 				break;
 			case 1:
 				if(cool_time > b_cooldown) can_attack = true;
-				break:
+				break;
 			case 2:
 				if(cool_time > x_cooldown) can_attack = true;
 				break;
 			case 3:
 				if(cool_time > y_cooldown) can_attack = true;
 				break;
-		}*/
-
+		}
+		
 		//Movement
 		var x = Input.GetAxis("LeftX") * Time.deltaTime * 10f;
 		var y = -Input.GetAxis("LeftY");
@@ -64,11 +62,13 @@ public class PlayerScript : MonoBehaviour {
 			if(Input.GetButtonDown("A") && !a_disabled){
 				last_used = 0;
 				can_attack = false;
+				cool_time = 0.0f;
 				Debug.Log("A attack used");
 			}
 			if(Input.GetButtonDown("B") && !b_disabled){
 				last_used = 1;
 				can_attack = false;
+				cool_time = 0.0f;
 				Debug.Log("B attack used");
 				Lazor proj = (Lazor)Instantiate(projectile, transform.position, Quaternion.identity);
 				proj.GetComponent<Rigidbody>().AddForce(500f, 0, 0);
@@ -76,11 +76,13 @@ public class PlayerScript : MonoBehaviour {
 			if(Input.GetButtonDown("X") && !x_disabled){
 				last_used = 2;
 				can_attack = false;
+				cool_time = 0.0f;
 				Debug.Log("X attack used");
 			}
 			if(Input.GetButtonDown("Y") && !y_disabled){
 				last_used = 3;
 				can_attack = false;
+				cool_time = 0.0f;
 				Debug.Log("Y attack used");
 			}
 		}
